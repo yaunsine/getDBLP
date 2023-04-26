8ae8486b1529fad807a2a5ff894098db6069fcec
yaunsine <1580909148@qq.com>
2023-04-26 10:46:48 +0800

    fix:修复authors数据异构问题

--- getPaper.py
+++ getPaper.py
@@ -13,11 +13,11 @@ import logging
 def print_input_arg(args):
     dict_args = dict(vars(args))
     for k in dict_args.keys():
-        print(f"{k}: {dict_args[k]}")
+        logging.info(f"###[{k}: {dict_args[k]}]###")
 
 logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=logging.DEBUG)
 parser = argparse.ArgumentParser()
-parser.add_argument("--keyword", default="gan", type=str, help="content will be searched in dblp")
+parser.add_argument("--keyword", default="recommend", type=str, help="content will be searched in dblp")
 parser.add_argument("--page_count", default=1000, type=int, help="get count per page")
 parser.add_argument("--output_csv_path", default="paperExcel/", type=str, help="csv save path")
 args = parser.parse_args()
@@ -58,7 +58,12 @@ for i, x in enumerate(cols_name):
         lss = [[] for _ in range(9)]
         for j, v1 in enumerate(ls[i]):
             try:
-                lss[0].append(",".join([x['text'] for x in v1['authors']['author']])) if 'authors' in v1 else lss[0].append(None)
+                if 'authors' not in v1:
+                    lss[0].append(None)
+                elif isinstance(v1['authors']['author'], dict):
+                    lss[0].append(v1['authors']['author']['text']) if 'authors' in v1 else lss[0].append(None)
+                else:
+                    lss[0].append(",".join([x['text'] for x in v1['authors']['author']])) if 'authors' in v1 else lss[0].append(None)
                 lss[1].append(v1['title']) if 'title' in v1 else lss[1].append(None)
                 lss[2].append(v1['venue']) if 'venue' in v1 else lss[2].append(None)
                 lss[3].append(v1['type']) if 'type' in v1 else lss[3].append(None)
@@ -68,8 +73,10 @@ for i, x in enumerate(cols_name):
                 lss[7].append(v1['url']) if 'url' in v1 else lss[7].append(None)
                 lss[8].append(v1['ee']) if 'ee' in v1 else lss[8].append(None)
             except Exception as e:
-                print(v1)
-                print(e)
+                # print(lss)
+                logging.warning(e)
+                logging.warning(v1)
+                # print(e)
         din = ['authors', 'title', 'venue', 'type', 'year', 'access', 'key', 'url', 'ee']
         for i, x in enumerate(din):
             data_dict[x] = lss[i]
